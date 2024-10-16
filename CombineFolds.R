640de79e0bb50e2fe31e1f6009fae2d14818cae0
user485584 <rutger.ballieux@hotmail.com>
2024-10-16 19:26:39 +0200

    Refactor CombineFolds.R

--- CombineFolds.R
+++ CombineFolds.R
@@ -1,5 +1,24 @@
+# Load Necessary Libraries
 library(magrittr)
+library(dplyr)
 
+
+# ----------------------------
+# Function: combine_folds
+# ----------------------------
+
+#' Combine Multiple Fold Results into Single Files
+#'
+#' This function reads multiple RDS files corresponding to different folds, combines them into a single data frame,
+#' and saves the combined results as both RDS and CSV files.
+#'
+#' @param FilePath String. Directory path where fold RDS files are located.
+#' @param from Integer. Starting fold number.
+#' @param to Integer. Ending fold number.
+#' @param by Integer. Step size for fold numbers.
+#' @param OutputPath String. Directory path to save the combined results.
+#'
+#' @return Combined data frame of all folds.
 combine_folds <- function(FilePath, from, to, by, OutputPath) {
   # Initialize an empty list to store the folds
   folds_list <- list()
@@ -15,7 +34,6 @@ combine_folds <- function(FilePath, from, to, by, OutputPath) {
   
   # Combine all folds into a single data frame
   combined_folds <- do.call(rbind, folds_list)
-  
   rownames(combined_folds) <- NULL
   
   # Save the combined data frame as an RDS file
@@ -28,6 +46,25 @@ combine_folds <- function(FilePath, from, to, by, OutputPath) {
 }
 
 
+
+# ----------------------------
+# Function: combine_folds_coef_importances
+# ----------------------------
+
+#' Combine Coefficients and Importances Across Feature Ranges
+#'
+#' This function reads coefficient and importance RDS files for specified feature ranges and combines them into single data frames.
+#' The combined results are saved as both RDS and CSV files.
+#'
+#' @param Disease String. Disease label.
+#' @param Analysis String. Type of analysis (in this case a Number of RandomFeatures).
+#' @param FilePath String. Directory path where coefficient and importance RDS files are located.
+#' @param from Integer. Starting feature range.
+#' @param to Integer. Ending feature range.
+#' @param by Integer. Step size for feature ranges.
+#' @param OutputPath String. Directory path to save the combined results.
+#'
+#' @return List containing combined coefficients and importances.
 combine_folds_coef_importances <- function(Disease, Analysis, FilePath, from, to, by, OutputPath) {
   # Initialize empty lists to store the coefficients and importances for each range of features
   coef_list <- list()
@@ -48,7 +85,7 @@ combine_folds_coef_importances <- function(Disease, Analysis, FilePath, from, to
     import_path <- paste0(FilePath, "Features_", j, "_to_", end, "importance_vals_Fold1.rds")
     
     
-    # Import the RDS files into separate variables
+    # Read and process coefficients
     coef_df <- readRDS(coef_path) %>% as.data.frame() %>% rownames_to_column()
     
     #modify dfs
@@ -62,6 +99,8 @@ combine_folds_coef_importances <- function(Disease, Analysis, FilePath, from, to
       select(Disease, Analysis, Fold, Classifier, gene, absimportance, importance) %>%
       arrange(-absimportance)
     
+    
+    # Read and process importances
     import_df <- readRDS(import_path) %>% as.data.frame() %>% rownames_to_column()
     
     import_df <- import_df %>% 
@@ -106,48 +145,53 @@ combine_folds_coef_importances <- function(Disease, Analysis, FilePath, from, to
 # )
 
 
+
+# ----------------------------
+# Function: combine_folds_coef_importances_nonrandomfeatures
+# ----------------------------
+
+#' Combine Coefficients and Importances for Non-Random Features Across Folds
+#'
+#' This function processes coefficient and importance RDS files for different analyses and combines them into single data frames.
+#' The combined results are saved as both RDS and CSV files.
+#'
+#' @param Disease String. Disease label.
+#' @param Analysis String. Type of analysis (e.g., "TopPCs", "VariableFeatures", "DEWGCNA").
+#' @param NrFolds Integer. Number of folds to process.
+#' @param FilePath String. Directory path where coefficient and importance RDS files are located.
+#' @param OutputPath String. Directory path to save the combined results.
+#'
+#' @return List containing combined coefficients and importances.
 combine_folds_coef_importances_nonrandomfeatures <- function(Disease, Analysis, NrFolds, FilePath, OutputPath){
   
   coef_list <- list()
   import_list <- list()
   
+  # Define feature sequences based on analysis type
+  FeatureSequence <- switch(
+    Analysis,
+    "TopPCs" = c("10PCs", "50PCs", "100PCs", "250PCs", "500PCs", "1000PCs"),
+    "VariableFeatures" = c(100, 500, 1000, 1500, 2000),
+    "DEWGCNA" = "",
+    stop("Error: Invalid value for 'Analysis'. Please provide one of 'TopPCs', 'VariableFeatures', or 'DEWGCNA'.")
+  )
+  
   for (Fold in 1:NrFolds){
-    
-    if (Analysis == "TopPCs"){
-      FeatureSequence <- c("10PCs", "50PCs", "100PCs", "250PCs", "500PCs", "1000PCs")
-      
-    } else if (Analysis == "VariableFeatures"){
-      FeatureSequence <- c(100, 500, 1000, 1500, 2000)
-      
-    } else if (Analysis == "DEWGCNA"){
-      FeatureSequence <- ""
-      
-    } else {
-      stop("Error: Invalid value for 'Analysis'. Please provide one of 'TopPCs', 'VariableFeatures', or 'DEWGCNA'.")
-    }
-    
-    
     for (NrFeatures in FeatureSequence) {
-      
-      if (Analysis == "TopPCs"){
-        prefix <- paste0("Top", NrFeatures)
-        
-      } else if (Analysis == "VariableFeatures"){
-        prefix <- paste0("Top", NrFeatures, Analysis)
-        
-      } else if (Analysis == "DEWGCNA"){
-        prefix <- Analysis
-        
-      } else {
-        stop("Error: Invalid value for 'Analysis'. Please provide one of 'TopPCs', 'VariableFeatures', or 'DEWGCNA'.")
-      }
+      prefix <- switch(
+        Analysis,
+        "TopPCs" = paste0("Top", NrFeatures),
+        "VariableFeatures" = paste0("Top", NrFeatures, Analysis),
+        "DEWGCNA" = Analysis
+      )
       
       coef_path <- paste0(FilePath, prefix, "coef_matrix_Fold", Fold, ".rds")
       import_path <- paste0(FilePath, prefix, "importance_vals_Fold", Fold, ".rds")
       
+      
+      # Read and process coefficients
       coef_df <- readRDS(coef_path) %>% as.data.frame() %>% rownames_to_column()
       
-      #modify dfs
       coef_df <- coef_df %>% 
         rename(gene = rowname, importance = s1) %>% 
         mutate(absimportance = abs(importance)) %>%
@@ -158,6 +202,8 @@ combine_folds_coef_importances_nonrandomfeatures <- function(Disease, Analysis, 
         select(Disease, Analysis, Fold, Classifier, gene, absimportance, importance) %>%
         arrange(-absimportance)
       
+      
+      # Read and process importances
       import_df <- readRDS(import_path) %>% as.data.frame() %>% rownames_to_column()
       
       import_df <- import_df %>% 
